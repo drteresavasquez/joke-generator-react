81eab43a236cd00c74d8b74ab4644673e669bd06
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-06 11:33:58 -0500

    added joke component

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -23,12 +23,28 @@ class Joke extends Component{
     }
 
     componentDidMount(){
-        console.log("Comp Did Mount")
         this.getJoke()
     }
 
     getJoke(){
         console.log("Get Joke()")
+        fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke')
+        .then(result => result.json())
+        .then(
+            (result) => {
+                console.log("RESULT", result);
+                this.setState({
+                    jokeLoaded: true,
+                    objResult: result,
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error,
+                })
+            }
+        )
     }
 
     render(){
@@ -36,9 +52,37 @@ class Joke extends Component{
         return (
             error ? <div>Error: {error.message}</div> : 
             !jokeLoaded ? <div>Loading...</div> : 
-            <div><h2>Got A Joke!</h2></div>
+            <div>
+                <JokeSetup 
+                    jokeLoaded={jokeLoaded} 
+                    jokeSetup={objResult.setup}
+                    jokeType={objResult.type}
+                    showResult={showResult}
+                />
+            </div>
         )
     }
+}
+
+function JokeSetup(props){
+    return(
+        <Card body inverse style={{ backgroundColor: '#85144b', borderColor: '#85144b' }}>
+         {props.jokeLoaded ?
+            <span>
+               <CardTitle>Joke About {props.jokeType.charAt(0).toUpperCase() + props.jokeType.slice(1)}</CardTitle>
+               <CardText>{props.jokeSetup}</CardText>
+               {/* <Button color="info" onClick={props.showClicked}>TELL ME</Button> */}
+            </span>
+            :
+            <CardTitle>Getting a Joke</CardTitle>
+         }
+         {props.showResult ?
+            <div> </div>
+            :
+            <Button color="info" onClick={() => { props.showClicked(props.index) }}>TELL ME</Button>
+         }
+      </Card>
+    )
 }
 
 export default Joke;
